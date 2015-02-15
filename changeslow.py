23c88e9cd2edd18b62e8731765176885a532a5e4
Bryon <bryonb@gmail.com>
2015-02-14 17:22:08 -0800

    Changeslow done

--- changeslow.py
+++ changeslow.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python
 
-import changegreedy as greedy
+def getchange(v, c):
+  #check if there is a coin of value c
+  found = False
+  for i, j in enumerate(v):
+    if j == c:
+      coins = [x*0 for x in range(len(v))]
+      coins[i] = 1
+      found = True
+      return coins, 1
+  if not found:
+    coins = [x*0 for x in range(len(v))]
+    minC = c
+    for i in range(c-1, 0, -1):
+      coins1, minC1 = getchange(v, i)
+      coins2, minC2 = getchange(v, c-i)
+      totalC = minC1 + minC2
+      if totalC < minC:
+        minC = totalC
+        coins = [coins1[x] + coins2[x] for x in range(len(v))]
+  return coins, minC
 
-v=[1,5, 10, 25]
-c = 240
-minC = c
-
-
-for k in range(c, 0, -1):
-  coinarray1, coins1 = greedy.getchange(v, k)
-  coinarray2, coins2 = greedy.getchange(v, c-k)
-
-  if (coins1+coins2) < minC:
-    minC = coins1 + coins2
-    minK = k
-    numC = [coinarray1[x] + coinarray2[x] for x in range(len(v))]
-
-return (numC, minC)
+print getchange([1, 5, 10, 25, 50], 15)
